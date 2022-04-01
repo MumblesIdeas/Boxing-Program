ec56b41766a14cccb077e55e2465bd8860db5ca3
J-Segundo <72644858+J-Segundo@users.noreply.github.com>
2022-03-31 19:38:53 -0700

    changed title a couple of files

--- Code/how many rounds.cpp
+++ Code/how many rounds.cpp
@@ -6,15 +6,26 @@
 //
 #include <iostream>
 #include "current rounds.hpp"
+int currentrounds (){
 
-void currentrounds (int rounds){
 
+    std::cout << "How many rounds is this fight?" << std::endl;
 
-    do {
-        std::cout << "How many rounds is this fight?" << std::endl;
-        std::cin >> rounds;
+    for (int i=1; i < rounds; i++) {
+        std::cout << "The current round is " << rounds << std::endl;
+
+        
+
 
     }
+
+
+
+
+
+
+
+
 
     while (rounds >= 1 && rounds <= 12); {
         std::cout << "This fight is scheduled for " << rounds << " rounds" << std::endl;
@@ -29,6 +40,28 @@ void currentrounds (int rounds){
             // How am I going to get the question above to repeat each time something is wrong? It ask the question twice but stops regardless if it is right or wrong.
    // }
 
-return 0;
+    while(int rounds; rounds<=12; rounds++; ) { // This helps compare three numbers...checks if we have rounds that are  between 1 and 12
+        cin >> rounds;
+        cout << "Who's in the Blue corner? ";
+    }
+    cin >> BlueCorner;
+    cout << "Who is in the Red corner? ";
+    cin >> RedCorner;
+}
+
+cin >> rounds;
+
+}
+
+*/
+
+    //  cout << "Who won round" << currentround << "?"; // Cool, so this displays regarless of the number we input.
+}
+
+
+
+
+
+
 
 }
